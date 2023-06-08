5103b2ca6e2d96376c34cbca6f3367e836402c54
Adrian Trobajo Clavel <atrobajo@gmail.com>
2023-06-08 14:21:12 -0400

    first commit

---
+++ party-control-api/src/main/java/com/leveltwo/partycontrol/PartyControlApplication.java
@@ -0,0 +1,13 @@
+package com.leveltwo.partycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyControlApplication.class, args);
+	}
+
+}
